9a6181eae30009e87176bb2aac4a1b1df7b7cd8a
Angel Vazquez <angel.vazquez@flagbit.de>
2017-08-30 11:02:21 +0200

    [SOF-3]  Update developer environment
    - Adapt PHP container to insert nginx ip and backend url in hosts file
    - Adapt configuration

--- .docker/php-ubuntu/docker-entrypoint.sh
+++ .docker/php-ubuntu/docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+if [ 1 -eq 1 ]; then
+ until nslookup fanshop-nginx | grep "Non-authoritative"
+ do
+   echo "Waiting...";
+ done
+   echo $(nslookup fanshop-nginx | grep Address | tail -1 | grep -o "[0-9.]*" | perl -ne 'print "$_ backend.spryker-fanshop.dev"') >> /etc/hosts
+
+fi
+
 php-fpm -F &
 /usr/sbin/sshd -D
--- config/Shared/config_local.dist.php
+++ config/Shared/config_local.dist.php
@@ -1,10 +1,15 @@
 <?php
 
+use Pyz\Shared\Newsletter\NewsletterConstants;
+use Spryker\Shared\Customer\CustomerConstants;
+use Spryker\Shared\Payolution\PayolutionConstants;
+use Spryker\Shared\ProductManagement\ProductManagementConstants;
 use Spryker\Shared\Propel\PropelConstants;
 use Spryker\Shared\Session\SessionConstants;
 use Spryker\Shared\Setup\SetupConstants;
 use Spryker\Shared\Application\ApplicationConstants;
 use Spryker\Shared\Search\SearchConstants;
+use Spryker\Shared\ZedRequest\ZedRequestConstants;
 
 // ---------- Propel
 $config[PropelConstants::PROPEL_DEBUG] = true;
@@ -35,7 +40,7 @@ $config[SearchConstants::ELASTICA_PARAMETER__PORT] = $ELASTICA_PORT;
 
 // ---------- Yves host
 $config[ApplicationConstants::HOST_YVES] = 'spryker-fanshop.dev';
-$config[ApplicationConstants::PORT_YVES] = '443';
+$config[ApplicationConstants::PORT_YVES] = ':443';
 $config[ApplicationConstants::PORT_SSL_YVES] = '';
 $config[ApplicationConstants::BASE_URL_YVES] = sprintf(
     'http://%s%s',
@@ -47,6 +52,30 @@ $config[ApplicationConstants::BASE_URL_SSL_YVES] = sprintf(
     $config[ApplicationConstants::HOST_YVES],
     $config[ApplicationConstants::PORT_SSL_YVES]
 );
+
+$config[ProductManagementConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
+$config[PayolutionConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
+$config[NewsletterConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
+$config[CustomerConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
+
+// ---------- Zed host
+$config[ApplicationConstants::HOST_ZED] = 'backend.spryker-fanshop.dev';
+$config[ZedRequestConstants::HOST_ZED_API] = $config[ApplicationConstants::HOST_ZED];
+$config[SessionConstants::ZED_SESSION_COOKIE_NAME] = $config[ApplicationConstants::HOST_ZED];
+
+$config[ApplicationConstants::BASE_URL_ZED] = sprintf(
+    'http://%s%s',
+    $config[ApplicationConstants::HOST_ZED],
+    $config[ApplicationConstants::PORT_ZED]);
+
+$config[ApplicationConstants::BASE_URL_SSL_ZED] = sprintf(
+    'https://%s%s',
+    $config[ApplicationConstants::HOST_ZED],
+    $config[ApplicationConstants::PORT_SSL_ZED]
+);
+$config[ZedRequestConstants::HOST_ZED_API] = $config[ApplicationConstants::HOST_ZED];
+$config[ZedRequestConstants::BASE_URL_ZED_API] = $config[ApplicationConstants::BASE_URL_ZED];
+$config[ZedRequestConstants::BASE_URL_SSL_ZED_API] = $config[ApplicationConstants::BASE_URL_SSL_ZED];
 
 // ---------- SSL
 $config[ApplicationConstants::YVES_SSL_ENABLED] = true;
